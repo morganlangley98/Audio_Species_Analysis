00f459c3e63daa20eef7c8c2cf2bd21047a8776b
Morgan <122368285+morganlangley98@users.noreply.github.com>
2023-10-04 11:41:23 +0100

    Update and rename BatDetect2 to CSV Batch.py to BatDetect2_installation.py

---
+++ BatDetect2_installation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# https://github.com/macaodha/batdetect2/blob/main/batdetect2_notebook.ipynb
+from batdetect2 import api, plot
+import os
+import librosa
+import soundfile
+import csv
